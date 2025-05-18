134d6b7398e84ab6988d21c476f4c6f8bea39e42
Pedro Carpes <pcarpes@gmail.com>
2025-05-18 11:53:32 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def home():
     return render_template('home.html')
 
