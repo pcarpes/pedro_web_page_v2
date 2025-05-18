f85adb78e5d7e5f21ff042606a0483eea1b0d049
Pedro Carpes <pcarpes@gmail.com>
2025-05-18 12:14:18 -0400

    Deploy Railway

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def home():
     return render_template('home.html')
 
